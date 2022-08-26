56dcf6fcfc19b245f73dc09ed47373d65d155fcc
Faizan Haider <hfaizan53@gmail.com>
2022-08-27 00:48:00 +0500

    setting up UI and routes

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+    def opt_out
+    end
+
+    def submit_data
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "pages#opt_out"
+
+  post "/submit_data", to: "pages#submit_data"
 end
