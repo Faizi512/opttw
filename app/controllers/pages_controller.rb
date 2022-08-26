caff622354ecfe09423cd54ea09d87f1ee3a2ff5
Faizan Haider <hfaizan53@gmail.com>
2022-08-27 02:00:37 +0500

    remove byebug

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,9 +3,6 @@ class PagesController < ApplicationController
     end
 
     def submit_data
-        
-        byebug
-
         url = URI.parse("http://dukeleads.leadbyte.co.uk/api/submit.php?returnjson=yes&campid=STOPUK&email=#{params[:email]}&phone1=#{params[:phone]}")
         req = Net::HTTP::Get.new(url.to_s)
         res = Net::HTTP.start(url.host, url.port) {|http|
